a28d3368c08ebdeb9daab73df8952e7ca8264419
susolr <jlopezpeque@gmail.com>
2023-12-04 11:24:32 +0100

    Creacion de la clase alimento

---
+++ app/Alimento.py
@@ -0,0 +1,7 @@
+# archivo: Alimento.py
+# autor: Jesús López Rodríguez
+# descripción: Este archivo contiene funciones para gestionar la clase Alimento
+
+class Alimento:
+    def __init__(self, nombre):
+        self.nombre = nombre
