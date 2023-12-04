8ea04a3ab03ed6db6016eddc5d6381576a5b2715
susolr <jlopezpeque@gmail.com>
2023-12-04 11:29:35 +0100

    Creacion de las clases receta y nevera

---
+++ app/Nevera.py
@@ -0,0 +1,7 @@
+# archivo: Receta.py
+# autor: Jesús López Rodríguez
+# descripción: Este archivo contiene funciones para gestionar la clase Nevera
+
+class Nevera:
+    def __init__(self):
+        self.alimentos = []
---
+++ app/Receta.py
@@ -0,0 +1,9 @@
+# archivo: Receta.py
+# autor: Jesús López Rodríguez
+# descripción: Este archivo contiene funciones para gestionar la clase Receta
+
+class Receta:
+    def __init__(self, nombre, ingredientes, preparacion):
+        self.nombre = nombre
+        self.ingredientes = ingredientes
+        self.preparacion = preparacion
